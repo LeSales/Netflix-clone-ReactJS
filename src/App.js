e4aa0d02b29978be9e3e7139c713aa7085b027cc
Leandro Sales <leandrossales.96@gmail.com>
2021-07-11 23:37:28 -0300

    Att link da logo e add ícones de redes sociais

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 import FeaturedMovie from "./components/FeaturedMovie";
 import MovieRow from "./components/MovieRow";
 import Tmdb from "./Tmdb";
-import Header from './components/Header'
+import Header from "./components/Header";
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 export default () => {
   const [movieList, setMovieList] = useState([]);
@@ -15,16 +17,17 @@ export default () => {
       let list = await Tmdb.getHomeList();
       setMovieList(list);
 
-      let originals = list.filter(i => i.slug === 'originals');
-      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let originals = list.filter((i) => i.slug === "originals");
+      let randomChosen = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      );
       let chosen = originals[0].items.results[randomChosen];
-      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, "tv");
       setFeaturedData(chosenInfo);
     };
 
     loadAll();
   }, []);
-
 
   useEffect(() => {
     const scrollListener = () => {
@@ -33,24 +36,20 @@ export default () => {
       } else {
         setBlackHeader(false);
       }
-    }
+    };
 
-    window.addEventListener('scroll', scrollListener);
+    window.addEventListener("scroll", scrollListener);
 
     return () => {
-      window.removeEventListener('scroll', scrollListener);
-    }
-
+      window.removeEventListener("scroll", scrollListener);
+    };
   }, []);
 
   return (
     <div className="page">
-
       <Header black={blackHeader} />
 
-      {featuredData &&
-        <FeaturedMovie item={featuredData} />
-      }
+      {featuredData && <FeaturedMovie item={featuredData} />}
 
       <section className="lists">
         {movieList.map((item, key) => (
@@ -58,17 +57,31 @@ export default () => {
         ))}
       </section>
 
-      <footer>
-        Feito por Leandro Sales<br />
-        Direitos de imagem para Netflix <br />
-        Dados pegos do site Themoviedb.org
+      <footer className="footer-text">
+        <div >
+          <p>Feito por Leandro Sales<br/>
+          Direitos de imagem para Netflix<br/>
+          Dados pegos do site Themoviedb.org<br/>
+          </p>
+        </div>
+        <div className="contacts">
+          <a href="https://www.linkedin.com/in/leandro-sales-87249059/" target="_blank" rel="noopener noreferrer">
+          <LinkedInIcon/>
+          </a>
+          <a href="https://github.com/LeSales" target="_blank" rel="noopener noreferrer">
+          <GitHubIcon/>
+          </a>
+        </div>
       </footer>
 
-      {movieList <= 0 &&
+      {movieList <= 0 && (
         <div className="loading">
-          <img src="https://www.filmelier.com/pt/br/news/wp-content/uploads/2020/03/netflix-loading.gif" alt="Carregando" />
+          <img
+            src="https://www.filmelier.com/pt/br/news/wp-content/uploads/2020/03/netflix-loading.gif"
+            alt="Carregando"
+          />
         </div>
-      }
+      )}
     </div>
   );
 };
